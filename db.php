e79138142470bfdeb7fbb4bc9362ee4de1ef8ad6
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-05-09 14:15:51 +0200

    Added: file db.php and included it on index.php

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+class Production {
+    public $title;
+    public $language;
+    public $vote;
+    
+    function __construct(string $_title,string $_language,int $_vote){
+        $this->title = $_title;
+        $this->language = $_language;
+        $this->vote = $_vote;
+    }
+}
+
+$avatar = new Production('Avatar', 'English', 9);
+$titanic = new Production('Titanic','English',10);
--- index.php
+++ index.php
@@ -1,22 +1,5 @@
 <?php
-
-class Production {
-    public $title;
-    public $language;
-    public $vote;
-    
-    function __construct(string $_title,string $_language,int $_vote){
-        $this->title = $_title;
-        $this->language = $_language;
-        $this->vote = $_vote;
-    }
-}
-
-$avatar = new Production('Avatar', 'English', 9);
-var_dump($avatar);
-
-$titanic = new Production('Titanic','English',10);
-var_dump($titanic);
+require_once __DIR__ . '/db.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
