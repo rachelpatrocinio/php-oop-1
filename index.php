71cd021006ef46181cf190841786707333fa126c
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-05-09 14:01:44 +0200

    Added: printed data on page

--- index.php
+++ index.php
@@ -17,5 +17,26 @@ var_dump($avatar);
 
 $titanic = new Production('Titanic','English',10);
 var_dump($titanic);
-
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PRODUCTIONS</title>
+</head>
+<body>
+    <ul>
+        <li>
+            <h4><?php echo $avatar->title;?></h4>
+            <p><?php echo $avatar->language;?></p>
+            <p><?php echo $avatar->vote;?></p>
+        </li>
+        <li>
+            <h4><?php echo $titanic->title;?></h4>
+            <p><?php echo $titanic->language;?></p>
+            <p><?php echo $titanic->vote;?></p>
+        </li>
+    </ul>
+</body>
+</html>
